c202af23778b4542d09431f4c37318cccc24f94c
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2019-04-16 21:40:56 +0300

    Added spiral matrix

---
+++ spiralMatrix.js
@@ -0,0 +1,60 @@
+function spiralMatrix(n) {
+  if (n === 0) {
+    return;
+  } else if (n === 1) {
+    return [n];
+  } else {
+    const result_array = [];
+  
+    for (let i = n; i > 0; i--) {
+      result_array.push([]);
+    }
+  
+    let counter = 1;
+    let total_elements = Math.pow(n, 2);
+
+    let startColumn = 0;
+    let endColumn = n - 1;
+
+    let startRow = 0;
+    let endRow = n - 1;
+
+    while (startColumn <= endColumn && startRow <= endRow) {
+      // top row
+      for (let i = startColumn; i <= endColumn; i++) {
+        result_array[startRow][i] = counter;
+        counter++;
+      }
+
+      startRow++;
+
+      // right column
+      for (let i = startRow; i <= endRow; i++) {
+        result_array[i][endColumn] = counter;
+        counter++;
+      }
+
+      endColumn--;
+
+      // bottom row
+      for (let i = endColumn; i >= startColumn; i--) {
+        result_array[endRow][i] = counter;
+        counter++;
+      }
+
+      endRow--;
+
+      // left column
+      for (let i = endRow; i >= startRow; i--) {
+        result_array[i][startColumn] = counter;
+        counter++;
+      }
+
+      startColumn++;
+    }
+
+    return result_array;
+  }
+}
+
+console.log(spiralMatrix(4));
