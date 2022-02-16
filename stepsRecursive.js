4b4039bc3d8c4e75b6c6e76d2e35db08bc4c0ff9
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2022-02-16 22:45:43 +0200

    add no recursion version

--- stepsRecursive.js
+++ stepsRecursive.js
@@ -12,3 +12,17 @@ function recursiveSteps(n, i = 1) {
 }
 
 recursiveSteps(5);
+
+// Without recursion
+
+function tower_builder(floors){
+  let space,star, tower = [];
+  for(i = 1; i <= floors; i++){
+    space = " ".repeat(floors - i);
+    star  = "*".repeat((2*i) - 1);
+    tower.push(`${space}${star}${space}`);
+  }
+  return tower;
+}
+
+console.log(tower_builder(4));
