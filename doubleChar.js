35060b79c6e129a95c610e8a06c1676dacaeefd4
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2022-06-22 13:47:50 +0300

    a pair of simple CodeWars Katas

---
+++ doubleChar.js
@@ -0,0 +1,11 @@
+function doubleChar(str) {
+  return str.split('').map(el => el + el).join('');
+}
+
+
+console.log(doubleChar("abcd"), "aabbccdd");
+console.log(doubleChar("Adidas"), "AAddiiddaass");
+console.log(doubleChar("1337"), "11333377");
+console.log(doubleChar("illuminati"), "iilllluummiinnaattii");
+console.log(doubleChar("123456"), "112233445566");
+console.log(doubleChar("%^&*("), "%%^^&&**((");
