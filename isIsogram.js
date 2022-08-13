0f57cc233391be927bfb179286128211f6af06a5
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2022-08-13 20:36:13 +0300

    updated solution

--- isIsogram.js
+++ isIsogram.js
@@ -1,12 +1,17 @@
+// function isIsogram(str) {
+//   let letters = str.toLowerCase().split('');
+//   let uniqueLetters = [];
+//   for (let i = 0; i < letters.length; i++) {
+//     if (!uniqueLetters.includes(letters[i])) {
+//       uniqueLetters.push(letters[i]);
+//     }
+//   }
+//   return uniqueLetters.length === letters.length;
+// }
+
+// More elegant solution:
 function isIsogram(str) {
-  let letters = str.toLowerCase().split('');
-  let uniqueLetters = [];
-  for (let i = 0; i < letters.length; i++) {
-    if (!uniqueLetters.includes(letters[i])) {
-      uniqueLetters.push(letters[i]);
-    }
-  }
-  return uniqueLetters.length === letters.length;
+  return new Set(str.toLowerCase()).size === str.length;
 }
 
 console.log(isIsogram("Dermatoglyphics"), true);
