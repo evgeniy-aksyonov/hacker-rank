addfa462f39f28395ceb14f87fe0a5320dd3b897
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2022-08-27 22:07:00 +0300

    rename fn

--- onlyNumbers.js
+++ onlyNumbers.js
@@ -1,15 +1,15 @@
-function filter_list(list) {
+function onlyNumbers(list) {
   return list.filter((item) => typeof item === 'number');
 }
 
-console.log(filter_list([1, 2, 'a', 'b']), [1, 2], 'For input [1,2,"a","b"]');
+console.log(onlyNumbers([1, 2, 'a', 'b']), [1, 2], 'For input [1,2,"a","b"]');
 console.log(
-  filter_list([1, 'a', 'b', 0, 15]),
+  onlyNumbers([1, 'a', 'b', 0, 15]),
   [1, 0, 15],
   'For input [1,"a","b",0,15]'
 );
 console.log(
-  filter_list([1, 2, 'aasf', '1', '123', 123]),
+  onlyNumbers([1, 2, 'aasf', '1', '123', 123]),
   [1, 2, 123],
   'For input [1,2,"aasf","1","123",123]'
 );
